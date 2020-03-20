4b8fdb813ea6a958a21e33210a29c38d1956ba0c
kouji tanaka <t.kouji1281@gmail.com>
2020-03-20 23:20:00 +0900

    任意メール一覧から添付ファイルの本体を取得しコード変換済み

--- gmail_main_attachment_200316.py
+++ gmail_main_attachment_200316.py
@@ -35,47 +35,42 @@ service = gmail_get_service()
 messages = service.users().messages()
 # 自分のメッセージ一覧を10件得る
 msg_list = messages.list(userId='me', maxResults=10).execute()
-pprint.pprint(msg_list)
+# pprint.pprint(msg_list)
+#上記のメッセージID一覧を取得（idとthreadId）
+msg_ID_list = msg_list['messages']
+pprint.pprint(msg_ID_list)
 
-# メッセージ一覧の最新の一つを得る
-msg = msg_list['messages'][0]
-id = msg['id']
-threadId = msg['threadId']
-# print(f'id:{id} threadId:{threadId}')
-
-#取得したメッセージ一覧の中に添付ファイルが存在するメッセージのIDをリストとして取得。
-def list_of_attachments_ID(msg_list):
-    list_of_attachments_ID= []
-    for msg in msg_list:
+#取得したメッセージ一覧の中に添付ファイルが存在するメッセージのID及びattachmentIdをリストとして取得。
+def dict_of_attachment_ID(msg_ID_list):
+    dict_of_attachment_ID= {}
+    for msg in msg_ID_list:
+        #メッセージIDを取得
         id = msg['id']
         # メッセージの本体を取得する
         data = messages.get(userId='me', id=id).execute()
+        
         try:
             if data['payload']['parts'][1]['body']['attachmentId']:
-                list_of_attachments_ID.append(id)
+                #attachmentIDを取得
+                attachment_ID = data['payload']['parts'][1]['body']['attachmentId']
+                dict_of_attachment_ID[id] = attachment_ID
         except KeyError:
             continue
-    return list_of_attachments_ID
+    return dict_of_attachment_ID
 
-print(list_of_attachments_ID(msg_list['messages']))
+pprint.pprint(dict_of_attachment_ID(msg_ID_list))
 
 
 import base64
 from googleapiclient import errors
 
-def GetAttachments(id,attachment_id):
-    # 添付ファイルのidを取得
-    # attachment_id = data['payload']['parts'][1]['body']['attachmentId']
-    for d in attachment_id:
+def GetAttachments(dict_of_attachment_ID):
+    for k,v in dict_of_attachment_ID.items():
         # 添付ファイルの本体を取得
-        # attachment = messages.attachments().get(userId='me',messageId = id, id=d).execute()
-        # # 添付ファイルのコードを変換
-        # file_data = base64.urlsafe_b64decode(attachment['data'])
-        # print(file_data)
-        print(d)
-        print(id)
-
-GetAttachments(id,list_of_attachments_ID(msg_list['messages']))
+        attachment = messages.attachments().get(userId='me',messageId = k, id=v).execute()
+        # 添付ファイルのコードを変換
+        file_data = base64.urlsafe_b64decode(attachment['data'])
+        print(file_data)
 
 
-
+GetAttachments(dict_of_attachment_ID(msg_ID_list))
