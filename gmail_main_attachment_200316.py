270aa1137f2b979f100674cd0f8588ba1a4235f6
kouji tanaka <t.kouji1281@gmail.com>
2020-04-03 17:26:02 +0900

    添付ファイルをフォルダへ保存できようにした。

--- gmail_main_attachment_200316.py
+++ gmail_main_attachment_200316.py
@@ -1,9 +1,12 @@
 #coding: utf-8
-import httplib2, os
+import httplib2 #os
 from googleapiclient import discovery
 from oauth2client import client, tools
 from oauth2client.file import Storage
+# from googleapiclient import errors
+import base64
 import pprint
+
 
 # Gmail権限のスコープを指定
 SCOPES = 'https://mail.google.com/'
@@ -33,16 +36,16 @@ service = gmail_get_service()
 
 # メッセージを扱うAPI
 messages = service.users().messages()
-# 自分のメッセージ一覧を10件得る
-msg_list = messages.list(userId='me', maxResults=10).execute()
+# 自分のメッセージ一覧を"maxResults"件得る
+msg_list = messages.list(userId='me', maxResults=30).execute()
 # pprint.pprint(msg_list)
-#上記のメッセージID一覧を取得（idとthreadId）
+#上記のメッセージID一覧を取得（idとthreadIdをリスト内の辞書として）
 msg_ID_list = msg_list['messages']
 pprint.pprint(msg_ID_list)
 
-#取得したメッセージ一覧の中に添付ファイルが存在するメッセージのID及びattachmentIdをリストとして取得。
-def dict_of_attachment_ID(msg_ID_list):
-    dict_of_attachment_ID= {}
+#添付ファイルを取得
+def GetAttachments(msg_ID_list):
+    # dict_of_attachment_ID= {}
     for msg in msg_ID_list:
         #メッセージIDを取得
         id = msg['id']
@@ -51,26 +54,20 @@ def dict_of_attachment_ID(msg_ID_list):
         
         try:
             if data['payload']['parts'][1]['body']['attachmentId']:
-                #attachmentIDを取得
+                #添付ファイルが存在するファイルに対し、attachmentIDを取得
                 attachment_ID = data['payload']['parts'][1]['body']['attachmentId']
-                dict_of_attachment_ID[id] = attachment_ID
+                # 添付ファイルの本体を取得
+                attachment = messages.attachments().get(userId='me',messageId = id, id=attachment_ID).execute()
+                # 添付ファイルのコードを変換
+                file_data = base64.urlsafe_b64decode(attachment['data'])
+                #path名＝添付ファイル名とする
+                print(f"添付ファイル名：{data['payload']['parts'][1]['filename']}")
+                path = data['payload']['parts'][1]['filename']
+                # open(path,"wb") as f: のpathはstrで./内にそのファイルが無くてもpath名で新規作成される。
+                #https://note.nkmk.me/python-file-io-open-with/ ←参考
+                with open(path,"wb") as f:
+                    f.write(file_data)
+
         except KeyError:
             continue
-    return dict_of_attachment_ID
-
-pprint.pprint(dict_of_attachment_ID(msg_ID_list))
-
-
-import base64
-from googleapiclient import errors
-
-def GetAttachments(dict_of_attachment_ID):
-    for k,v in dict_of_attachment_ID.items():
-        # 添付ファイルの本体を取得
-        attachment = messages.attachments().get(userId='me',messageId = k, id=v).execute()
-        # 添付ファイルのコードを変換
-        file_data = base64.urlsafe_b64decode(attachment['data'])
-        print(file_data)
-
-
-GetAttachments(dict_of_attachment_ID(msg_ID_list))
+GetAttachments(msg_ID_list)
